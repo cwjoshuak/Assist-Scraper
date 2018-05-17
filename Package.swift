abf3b59d19af257a7ede4fec475638539bfbde57
cwjoshuak <jkuan1997@gmail.com>
2018-05-17 10:41:51 -0700

    removed alamofire as dependency

--- Package.swift
+++ Package.swift
@@ -9,13 +9,12 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.1"),
-    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AssistScraper",
-            dependencies: ["SwiftSoup", "Alamofire"]),
+            dependencies: ["SwiftSoup"]),
     ]
 )
