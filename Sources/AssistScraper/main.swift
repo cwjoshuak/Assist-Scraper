b7554ceaa1fa963fc3584e0ad1d26f8e7476634a
cwjoshuak <jkuan1997@gmail.com>
2018-05-17 22:59:32 -0700

    agreement parsing not done

--- Sources/AssistScraper/main.swift
+++ Sources/AssistScraper/main.swift
@@ -44,7 +44,7 @@ do {
 // parse second page for destination campuses
 // string to be changed
 let iKeys = origin.keys
-let secondPage = URLComponents(string: welcomeBase!.string! + iKeys.first!)
+let secondPage = URLComponents(string: welcomeBase!.string! + "prompt.do?ia=FOOTHILL&ay=16-17"/*iKeys.first!*/)
 
 do {
     print("Parsing \(secondPage!.string!)")
@@ -72,12 +72,12 @@ do {
 // parse third page for majors
 // string to be changed
 let tKeys = destination.keys
-let thirdPage = URLComponents(string: welcomeBase!.string! + tKeys.first!)
+let thirdPage = URLComponents(string: welcomeBase!.string! + "articulationAgreement.do?inst1=none&inst2=none&ia=FOOTHILL&ay=16-17&oia=UCB&dir=1"/*tKeys.first!*/)
 
 do {
     print("Parsing \(thirdPage!.string!)")
-    let url = try String(contentsOf: thirdPage!.url!, encoding: .ascii)
-    let els: Elements? = try SwiftSoup.parse(url).select("form")
+    let url = try String(contentsOf: thirdPage!.url!, encoding: .ascii) // html of third page
+    let els: Elements? = try SwiftSoup.parse(url).select("form") // get all with "form"
     if let els = els {
         let elem = try els.select("select").array()
         for options in elem[3].children() {
@@ -104,15 +104,15 @@ let report = "http://web2.assist.org/cgi-bin/REPORT_2/Rep2.pl?"
 //aay=16-17&oia=UCB&dora=BUS%20ADM&ay=16-17&event=19&agreement=aa&ria=UCB&sia=DAC&ia=DAC&dir=1&&sidebar=false&rinst=left&mver=2&kind=5&dt=2"
 let queries = thirdPage!.queryItems!
 var params: [URLQueryItem] = [
-    URLQueryItem(name: "aay", value: queries[3].value), // aay is year agreement for transferring from
-    URLQueryItem(name: queries[4].name, value: queries[4].value), // oia is same as ria - transferring to
-    URLQueryItem(name: "dora", value: "PHYS" /*major*/), // dora is major
-    URLQueryItem(name: "ay", value: queries[3].value), // ay is year agreement for transferring to
+    URLQueryItem(name: "aay", value: queries[3].value), // aay is year agreement for origin
+    URLQueryItem(name: queries[4].name, value: queries[4].value), // oia is same as ria - destination
+    URLQueryItem(name: "dora", value: "CIV ENG" /*major*/), // dora is major - destination
+    URLQueryItem(name: "ay", value: queries[3].value), // ay is year agreement for destinatoin
     URLQueryItem(name: "event", value: "19"),
     URLQueryItem(name: "agreement", value: "aa"),
-    URLQueryItem(name: "ria", value: queries[4].value), // ria is same as oia - transferring to
-    URLQueryItem(name: "sia", value: queries[2].value), // sia is same as ia - transferring from
-    URLQueryItem(name: "ia", value: queries[2].value), // ia is same as sia - transferring from
+    URLQueryItem(name: "ria", value: queries[4].value), // ria is same as oia - destination
+    URLQueryItem(name: "sia", value: queries[2].value), // sia is same as ia - origin
+    URLQueryItem(name: "ia", value: queries[2].value), // ia is same as sia - origin
     URLQueryItem(name: "dir", value: queries[5].value),
     URLQueryItem(name: "&sidebar", value: "false"),
     URLQueryItem(name: "rinst", value: "left"),
@@ -120,27 +120,39 @@ var params: [URLQueryItem] = [
     URLQueryItem(name: "kind", value: "5"),
     URLQueryItem(name: "dt", value: "2")
 ]
+// ignore all the warnings thanks
 var agreementPage = URLComponents(string: report)
 agreementPage?.queryItems = params
 print(agreementPage!.url)
 
 do {
-    
     let html = try String(contentsOf: agreementPage!.url!, encoding: .ascii)
     let doc: Document = try SwiftSoup.parseBodyFragment(html)
     let body: Element? = doc.body()
+    let x = body?.children()
     let text = try body?.text()
     //print(text)
     var arr = text!.split(separator: "\n")
     var count = 0
 
+    let str = "--------------------------------------------------------------------------------"
+
+    /*
     for i in arr {
-        if !arr[count].contains("|") || !arr[count].contains("|") {
+        
+        if (!i.contains("|")) && (!i.contains(str)) {
+            
             arr.remove(at: count)
             count -= 1
+        } else {
+            print("didn't remove \(i) \(i.count) \(str.count)")
         }
+        
         count += 1
-    }
+    }*/
+    /*for i in t {
+        print(i)
+    }*/
     for i in arr {
         print(i)
     }
